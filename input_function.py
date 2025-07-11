01cd0f7e553f5a4e68a8e898a901cd25544fabef
Soniyathanniru <soniyathanniru1@gmail.com>
2025-07-11 16:01:15 +0530

    init

---
+++ input_function.py
@@ -0,0 +1,6 @@
+#input("what is your name?")
+#print("hi"+" "+input("what is your name?"))
+#ASSIGNMENT-1;
+#what is your name? 
+#hey soni, how are you?
+print("hey"+" "+input("what is your name? ")+","+ "how are you?")

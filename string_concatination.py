70d5f739583655c84549007ce9095ccc8969855c
Soniyathanniru <soniyathanniru1@gmail.com>
2025-07-11 15:20:04 +0530

    init

---
+++ string_concatination.py
@@ -0,0 +1,14 @@
+print("soni\nsoni\nsoni")
+#method-2\\
+print("hello"+"soni")
+#method-3\\
+print("hello"+" "+"soni")
+
+#indentation error:
+ #  print("hi"+"soni") suposs we are writing like this it will shown indentation error
+#assignment-1:
+#string manipulation exercise
+#string concatination is done,with"+"sign
+#e.g print("hello"+"jerry")
+#new lines can be created with a backslash and n:
+print('string manipulation exercise\nstring concatination is done,with"+"sign\ne.g print("hello"+"jerry")') 
